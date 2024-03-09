2e8e0ce55207e89d2693294b598e0215e0a78f1c
oleksandrpetrov <oleksandr.petrov@adastra.one>
2024-03-09 01:19:23 +0100

    Add detekt compose, formatting plugins

--- androidApp/src/main/java/com/example/news/android/MainActivity.kt
+++ androidApp/src/main/java/com/example/news/android/MainActivity.kt
@@ -17,7 +17,7 @@ class MainActivity : ComponentActivity() {
             MyApplicationTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
                     GreetingView(Greeting().greet())
                 }
