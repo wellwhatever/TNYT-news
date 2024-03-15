ce892bb2e3362c5e05996ba0c94f7aeb15945b4c
oleksandrpetrov <oleksandr.petrov@adastra.one>
2024-03-15 13:12:04 +0100

    Add AndroidComposeNavigationConventionPlugin

--- build-logic/convention/src/main/kotlin/AndroidComposeNavigationConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidComposeNavigationConventionPlugin.kt
@@ -1,18 +1,28 @@
 import com.example.news.versionCatalog
+import com.google.devtools.ksp.gradle.KspExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.dependencies
 
 class AndroidComposeNavigationConventionPlugin : Plugin<Project> {
 
     override fun apply(target: Project) {
         with(target) {
-            pluginManager.apply("com.google.devtools.ksp")
+            with(pluginManager) {
+                apply("com.google.devtools.ksp")
+                apply("androidx.navigation.safeargs.kotlin")
+            }
 
             val libs = versionCatalog()
             dependencies {
                 "implementation"(libs.findBundle("navigation").get())
-                "ksp"(libs.findBundle("navigation-ksp").get())
+                "ksp"(libs.findLibrary("compose-destinations-ksp").get())
+            }
+
+            extensions.configure<KspExtension> {
+                arg("compose-destinations.mode", "navgraphs")
+                arg("compose-destinations.moduleName", target.name)
             }
         }
     }
