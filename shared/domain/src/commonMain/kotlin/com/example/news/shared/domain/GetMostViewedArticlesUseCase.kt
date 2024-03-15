a30f61d2e18c63878a6b4ffcf7a9fb988b091aa3
oleksandrpetrov <oleksandr.petrov@adastra.one>
2024-03-15 13:14:27 +0100

    Add GetMostViewedArticlesUseCase

---
+++ shared/domain/src/commonMain/kotlin/com/example/news/shared/domain/GetMostViewedArticlesUseCase.kt
@@ -0,0 +1,15 @@
+package com.example.news.shared.domain
+
+import com.example.news.shared.code.data.ArticlesRepository
+import com.example.news.shared.code.model.Article
+
+class GetMostViewedArticlesUseCase(
+    private val articlesRepository: ArticlesRepository
+) {
+    suspend operator fun invoke(period: Int = DEFAULT_ARTICLES_PERIOD): List<Article> =
+        articlesRepository.getMostViewedArticles(period)
+
+    companion object {
+        const val DEFAULT_ARTICLES_PERIOD = 7
+    }
+}
