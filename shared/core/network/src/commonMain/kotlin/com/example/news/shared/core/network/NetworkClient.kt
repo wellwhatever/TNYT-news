92f5fef7ea6a8ad939371cd0547c7b3e2fe6b50a
oleksandrpetrov <oleksandr.petrov@adastra.one>
2024-03-17 14:22:06 +0100

    Setup logging, content negotiation

--- shared/core/network/src/commonMain/kotlin/com/example/news/shared/core/network/NetworkClient.kt
+++ shared/core/network/src/commonMain/kotlin/com/example/news/shared/core/network/NetworkClient.kt
@@ -3,25 +3,54 @@ package com.example.news.shared.core.network
 import io.ktor.client.HttpClient
 import io.ktor.client.call.body
 import io.ktor.client.plugins.HttpSend
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
 import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.plugins.logging.DEFAULT
+import io.ktor.client.plugins.logging.LogLevel
+import io.ktor.client.plugins.logging.Logger
+import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.plugin
 import io.ktor.client.request.HttpRequestBuilder
 import io.ktor.client.request.request
 import io.ktor.client.statement.HttpResponse
+import io.ktor.http.HttpHeaders
 import io.ktor.http.HttpMethod
 import io.ktor.http.isSuccess
+import io.ktor.serialization.kotlinx.json.json
+import kotlinx.serialization.json.Json
 
 class NetworkClient(
     private val baseUrl: String,
+    private val apiKey: String,
     httpClient: HttpClient,
 ) {
     val httpClientInternal = httpClient.config {
+        install(Logging) {
+            logger = Logger.Companion.DEFAULT
+            level = LogLevel.ALL
+            filter { request ->
+                request.url.host.contains("ktor.io")
+            }
+            sanitizeHeader { header -> header == HttpHeaders.Authorization }
+        }
+        install(ContentNegotiation) {
+            json(
+                Json {
+                    ignoreUnknownKeys = true
+                    prettyPrint = true
+                    isLenient = true
+                }
+            )
+        }
+
         defaultRequest {
             url(baseUrl)
         }
     }.apply {
         plugin(HttpSend).intercept { request ->
-            // fixme For testing purposes only
+            request.url {
+                parameters.append("api-key", apiKey)
+            }
             execute(request)
         }
     }
