63e3d52064769f2bb83c516ef61ce72f142d50c8
oleksandrpetrov <oleksandr.petrov@adastra.one>
2024-04-08 21:36:03 +0200

    Increment version name

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.news.android"
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.1"
     }
     signingConfigs {
         create("release") {
