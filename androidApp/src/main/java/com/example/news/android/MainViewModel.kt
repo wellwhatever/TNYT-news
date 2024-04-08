ee85d313baacc961760d743ee27d06b96f1f7810
oleksandrpetrov <oleksandr.petrov@adastra.one>
2024-04-08 16:38:49 +0200

    Add README.MD

--- androidApp/src/main/java/com/example/news/android/MainViewModel.kt
+++ androidApp/src/main/java/com/example/news/android/MainViewModel.kt
@@ -1,23 +1,17 @@
 package com.example.news.android
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
+import com.example.news.shared.core.common.stateInVmWithInitial
 import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.stateIn
 
 class MainViewModel : ViewModel(), MainActivityActions {
     private val showSnackbarFlow = MutableStateFlow(false)
 
     val state: StateFlow<MainActivityState> = showSnackbarFlow.map {
         MainActivityState(it)
-    }.stateIn(
-        viewModelScope,
-        SharingStarted.WhileSubscribed(5000),
-        initialValue = MainActivityState(false),
-    )
+    }.stateInVmWithInitial(MainActivityState(false))
 
     override fun showSnackbar() {
         showSnackbarFlow.value = true
