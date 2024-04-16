6cb14343af3b2073827e90ea90f784a5a31c412d
oleksandrpetrov <oleksandr.petrov@adastra.one>
2024-04-16 10:48:15 +0200

    Add unit tests for error state of ArticleListViewModel

--- feature/article/src/test/java/com/example/news/feature/article/list/ArticleListViewModelTest.kt
+++ feature/article/src/test/java/com/example/news/feature/article/list/ArticleListViewModelTest.kt
@@ -74,23 +74,42 @@ class ArticleListViewModelTest : FunSpec({
             awaitItem() // Ignore initial state
             viewModel.onQueryChange(newQuery)
 
+            // Only app bar state should be updated
             val result1 = awaitItem()
             val expected1 =
                 ArticleListScreenState.Content(articleListContentState, articleListUpdatedState)
 
             result1 shouldBe expected1
 
+            // Content state is set to loading while updating data
             val result2 = awaitItem()
             val expected2 =
                 ArticleListScreenState.Content(articleListLoadingState, articleListUpdatedState)
 
             result2 shouldBe expected2
 
+            // Both states should be set to content
             val result3 = awaitItem()
             val expected3 =
                 ArticleListScreenState.Content(articleListContentState, articleListUpdatedState)
 
             result3 shouldBe expected3
+        }
+    }
+
+    test("onClearQuery should reset query flow to empty string") {
+        viewModel.articleListScreenState.test {
+            viewModel.onQueryChange("expected")
+            viewModel.onClearQueryClick()
+            val result = expectMostRecentItem()
+
+            val expectedArticleListState =
+                ArticleListState.Content(FakeArticleDataProvider.fakeArticles.toImmutableList())
+            val expectedSearchBarState = ArticleSearchBarState("")
+            val expected =
+                ArticleListScreenState.Content(expectedArticleListState, expectedSearchBarState)
+
+            result shouldBe expected
         }
     }
 })
@@ -121,6 +140,20 @@ class ArticleListViewModelErrorTest : FunSpec({
                 ArticleListScreenState.Content(expectedArticleListState, expectedSearchBarState)
 
             result shouldBe expected
+        }
+    }
+
+    test("onReloadClick clear error state and refresh state") {
+        viewModel.articleListScreenState.test {
+            viewModel.onReloadClick()
+            val result = expectMostRecentItem()
+            val expectedListState = ArticleListState.Loading
+            val expectedSearchBarState = ArticleSearchBarState("")
+            val expected =
+                ArticleListScreenState.Content(expectedListState, expectedSearchBarState)
+
+            result shouldBe expected
+
         }
     }
 })
